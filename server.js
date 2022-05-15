e86f6cedbf8be3a3bb11af5d1ce08ffcb6d81c97
yehya-aljammal <yehe-y7@hotmail.com>
2022-05-15 23:49:41 +0300

    commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html')
+})
+  
+  app.post('/login', (req, res) => {
+   const { username, password }  = req.body;
+   if (username === "yehia" && password === "123") {
+       res.redirect("/home")
+   } else {
+       res.send("invalid password")
+   }
+  })
+  
+  app.get('/home', (req, res) => {
+    res.send("welcome")
+   })
+   
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
